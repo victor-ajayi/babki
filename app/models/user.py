34e8c733bd11664ee3be53cfc66e49e4945cbaec
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-02-15 03:03:17 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy.sql import func
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.extensions import db
 
@@ -10,7 +11,7 @@ class User(db.Model):
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username: str = db.Column(db.String, nullable=False)
     email: str = db.Column(db.String)
-    password_hash: str = db.Column(db.String, nullable=False)
+    password: str = db.Column(db.String, nullable=False)
     cash: float = db.Column(db.Float, server_default="0.0")
     savings: float = db.Column(db.Float, server_default="0.0")
     debt: float = db.Column(db.Float, server_default="0.0")
@@ -22,10 +23,18 @@ class User(db.Model):
     )
     updated_at: TIMESTAMP = db.Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
-    def __init__(self, username, password_hash) -> None:
+    def __init__(self, **kwargs) -> None:
         super().__init__()
-        self.username = username
-        self.password_hash = password_hash
+        self.__dict__.update(kwargs)
 
     def __repr__(self):
         return f"<User {self.id}: {self.username}>"
+
+    @property
+    def password(self):
+        raise AttributeError("password is write-only")
+
+    # Stores password in hashes
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
