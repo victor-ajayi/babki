94d190a5c3fff682c934514b0098041e935e337a
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-02-15 17:44:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 load_dotenv()
 
+DB_PASS = os.environ.get("DB_PASS")
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    # SQLALCHEMY_DATABASE_URI = (
-    #     os.environ.get(
-    #         "DATABASE_URI"
-    #         # ) or "sqlite:///" + os.path.join(basedir, "babki.db")
-    #     )
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URI")
-        or "postgresql://postgres:" + os.environ.get("DB_PASS") + "@localhost/babki"
-    )
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{DB_PASS}@localhost/babki"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
